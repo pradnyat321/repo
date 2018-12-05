00076c7abdc292bb8d45d6ac1c05e4e42f5695b5
Pradnya Takawale <pradnya.takawale@oracle.com>
2018-12-05 23:40:12 +0530

    comit prone added

---
+++ prone/src/main/java/grp1/prone/App.java
@@ -0,0 +1,17 @@
+package grp1.prone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int j =0;
+    	for (int i =0; i< 3;i++) {
+    		System.out.println( "Hello World!"+i );
+    	}
+    	
+    }
+}
